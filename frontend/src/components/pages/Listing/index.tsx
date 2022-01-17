1cb17bed8a1d44a6d09ce5f1c8a51b5d89aee884
carloseduardo95 <carlos.softdev@gmail.com>
2022-01-16 21:26:28 -0300

    First request

--- frontend/src/components/pages/Listing/index.tsx
+++ frontend/src/components/pages/Listing/index.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import Pagination from "../../Pagination";
 import MovieCard from "../../MovieCard";
+import axios from "axios";
+import {BASE_URL} from "../../../utils/requests";
 
 function Listing() {
+
+    // FORMA ERRADA
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+           console.log(response.data)
+        });
+
     return(
         <>
             <Pagination />
